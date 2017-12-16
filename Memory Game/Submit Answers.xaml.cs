bb581b4b9c162e66dfa2f8827d8adbad91ef91b8
unknown <mm101006@gmail.com>
2017-12-15 23:52:41 -0500

    feat: User is able to verify answers are correct

--- Memory Game/Submit Answers.xaml.cs
+++ Memory Game/Submit Answers.xaml.cs
@@ -22,24 +22,40 @@ namespace Memory_Game
         public Submit_Answers()
         {
             InitializeComponent();
-            //foreach (string word in ArrayParameters.RandomWords)
-            //{
-            //    MessageBox.Show(word);
-            //}
+        }
 
+        int count = 0;
+        public string CompareAnswer(string txtBoxAnswer,  string arrAnswer)
+        {
+            if (textBox.Text.ToLower().Trim() == ArrayParameters.RandomWords[count].ToLower().Trim())
+            {
+                MessageBoxResult output = MessageBox.Show("Correct");
+                count += 1;
+                if (GameParameters.WordCount == count)
+                {
+                    MessageBoxResult end = MessageBox.Show("Game Ended");
+                    MainWindow previousWindow = new MainWindow();
+                    previousWindow.Show();
+                    this.Close();
+                    return end.ToString();
+                } 
+                else
+                {
+                    textBox.Clear();
+                    return output.ToString();
+                }                
+            }
+            else
+            {
+                MessageBoxResult output = MessageBox.Show("Wrong");
+                return output.ToString();
+            }
         }
 
         private void button_Click_1(object sender, RoutedEventArgs e)
         {
-            
-            if (textBox.Text.ToLower().Trim() == ArrayParameters.RandomWords[0].ToLower().Trim())
-            {
-                MessageBox.Show("Correct");
-            }
-            else
-            {
-                MessageBox.Show("Wrong");
-            }           
+            CompareAnswer(textBox.Text.ToLower().Trim(), ArrayParameters.RandomWords[count].ToLower().Trim());
+                                    
         }
     }
 }
